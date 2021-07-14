d841084bcd00508cc0a6f7fc7ce101c5b914c4fb
Ko van der Sloot <Ko.vanderSloot@ziggo.nl>
2021-07-14 14:29:18 +0200

    next attempt

--- testaction.sh
+++ testaction.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Running testaction script!"
-./testall.sh
-export TEST_STAT=$?
+. ./testall.sh
+TEST_STAT=$?
+echo "set environment=$TEST_STAT!"
 echo "::set-output name=status::$TEST_STAT"
 exit 0
